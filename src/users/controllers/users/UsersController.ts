568e2e472ef5f2e62ecf32f108d707312cc4c94b
Cakirmuhittin <muhit@DESKTOP-8RUOIPD>
2024-01-29 17:01:38 +0300

    firstep

---
+++ src/users/controllers/users/UsersController.ts
@@ -0,0 +1,57 @@
+import { Body, Controller, Get, Param, Post, Req, Res } from '@nestjs/common';
+import { Request, Response, response } from 'express';
+import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
+
+
+@Controller('users')
+export class UsersController {
+
+    @Get()
+    getUsers() {
+        return { username: 'Anson', email: 'anson@anson.com' };
+    }
+
+    @Get('posts')
+    getUsersPosts() {
+        return [{
+            username: 'Anson',
+            email: 'anson@anson.com',
+            posts: [
+                {
+                    id: 1,
+                    title: 'Post 1',
+                },
+                {
+                    id: 2,
+                    title: 'Post 2',
+                },
+            ],
+        },];
+    }
+
+    @Get('posts/comments')
+    getUsersPostsComments() {
+        return [{
+            id: 1,
+            title: 'Post 1',
+            comments: [],
+        },];
+    }
+
+    // @Post()
+    // createUser(@Req() request: Request , @Res() response:Response){
+    //   console.log(request.body); 
+    //   response.send('Created');
+    // }
+    @Post('create')
+    createUser(@Body() userData: CreateUserDto) {
+        console.log(userData);
+        return {}
+    }
+
+    @Get(':id/:postId')
+    getUserById(@Param('id') id:string , @Param('postId') postId:string) {
+        console.log(id);
+        return { id,postId }
+    }
+}
